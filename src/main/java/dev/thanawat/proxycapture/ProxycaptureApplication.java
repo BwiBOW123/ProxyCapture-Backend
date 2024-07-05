3f9a3c812526d67a581dcbb9dd412cece01d9c75
BwiBOW123 <thanawatkheawkajee@gmail.com>
2024-07-05 15:55:04 +0700

    add HolderService

--- src/main/java/dev/thanawat/proxycapture/ProxycaptureApplication.java
+++ src/main/java/dev/thanawat/proxycapture/ProxycaptureApplication.java
@@ -1,13 +1,17 @@
 package dev.thanawat.proxycapture;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProxycaptureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProxycaptureApplication.class, args);
+		SpringApplication app = new SpringApplication(ProxycaptureApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
+		ConfigurableApplicationContext ctx = app.run(args);
 	}
-
 }
